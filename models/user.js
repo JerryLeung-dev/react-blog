4cb9bd8ce2d23fb611ece7f3a9488f11e3c2974c
Du Luong <luongvidu@gmail.com>
2020-06-30 17:32:59 +1000

    Hash a password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = mongoose.Schema({
     firstname: {
@@ -28,6 +29,25 @@ const UserSchema = mongoose.Schema({
     tokenExp: {
         type: Number
     }
+})
+
+UserSchema.pre('save', function(next){
+    const user = this;
+    console.log(user);
+    const saltRounds = 10;
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err);
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+                user.password = hash;
+                next(); //I add it myself
+            })
+        })
+    } else {
+        next();
+    }
+    
 })
 
 const User = mongoose.model('User', UserSchema);
