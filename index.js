22bc3bba9fe09906d37614d3cedb7c070ba7d4f9
Du Luong <luongvidu@gmail.com>
2020-06-30 16:52:09 +1000

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const {User} = require('./models/user');
 
 const app = express();
+const config = require('./config/key')
 
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://adminUser:Luongvidu123@react-blog.ezdts.mongodb.net/REACT-BLOG?retryWrites=true&w=majority',
+mongoose.connect(config.mongoURI,
                     {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>console.log('DB connected'))
                                             .catch(err => console.log(err))
+
+mongoose.set('useCreateIndex', true); //o have mongooose call the createIndex method on the mongodb native driver.
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json()); //convert req.body to json
+app.use(cookieParser());
+
 app.get('/', (req,res,next) =>{
     res.send('Hello World');
+})
+
+app.post('/api/users/register', (req,res) => {
+    const user = new User(req.body); //new document
+    user.save((err, userData) => { //save the document
+        if(err) return res.json({success: false, err});
+        return res.status(200).json({
+            success:true
+        });
+    });  
 })
 
 app.listen(5000);
