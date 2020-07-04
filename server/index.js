d71a6da5ee6473ec871cf65fdeb70c6da1482e71
Du Luong <luongvidu@gmail.com>
2020-07-04 17:16:10 +1000

    First heroku commit

---
+++ server/index.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const {User} = require('./models/user');
+const { auth } = require('./middlewares/auth');
+
+const app = express();
+const config = require('./config/key');
+// const user = require('./models/user');
+
+mongoose.connect(config.mongoURI,
+                    {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>console.log('DB connected'))
+                                            .catch(err => console.log(err))
+
+mongoose.set('useCreateIndex', true); //o have mongooose call the createIndex method on the mongodb native driver.
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json()); //convert req.body to json
+app.use(cookieParser());
+
+app.get('/', (req,res,next) =>{
+    res.send('Hello World');
+})
+
+app.get('/api/user/auth', auth, (req, res) => {
+    res.status(200)
+        .json({
+            _id: req.user._id,
+            isAuth: true,
+            email: req.user.email,
+            firstname: req.user.firstname,
+            lastname: req.user.lastname,
+            role: req.user.role
+        })
+})
+
+app.post('/api/users/register', (req,res) => {
+    const user = new User(req.body); //new document
+    user.save((err, userData) => { //save the document
+        if(err) return res.json({success: false, err});
+        return res.status(200).json({
+            success:true,
+            userData: userData
+        });
+    });  
+})
+
+app.post('/api/user/login', (req, res) => {
+    //Find email
+    User.findOne({email: req.body.email}, (err, user) => {
+        if(!user){
+            return res.json({
+                loginSuccess: false,
+                message: "Email not found"
+            })
+        }
+        //Compare password
+    user.comparePassword(req.body.password, (err, isMatch) =>{
+        if(!isMatch){
+            return res.json({
+                loginSuccess: false,
+                message: "Password is not correct"
+            })
+        }
+    })
+    //generateToken
+    user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+        res.cookie('x_auth', user.token)
+            .status(200)
+            .json({
+                loginSuccess: true,
+                user: user
+            })
+    })
+    })
+})
+//have to log in before log out , so we have to use the auth middleware
+app.get('/api/user/logout',auth, (req, res) => {
+    //we log user out by remove their token
+    User.findOneAndUpdate({_id:req.user._id},{token:""},{new: true, useFindAndModify: false}, (err, doc)=>{
+        if(err) return res.json({success: false, err});
+        return res.status(200)
+                    .json({
+                        success: true,
+                        // doc
+                    });
+    })
+})
+
+const port = process.env.PORT || 5000
+app.listen(port, () =>{
+    console.log(`Server running at ${port}`);
+});
--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = mongoose.Schema({
     firstname: {
@@ -33,7 +34,7 @@ const UserSchema = mongoose.Schema({
 
 UserSchema.pre('save', function(next){
     const user = this;
-    console.log(user);
+    // console.log(user);
     const saltRounds = 10;
     if(user.isModified('password')){
         bcrypt.genSalt(saltRounds, function(err, salt){
@@ -49,6 +50,34 @@ UserSchema.pre('save', function(next){
     }
     
 })
+
+UserSchema.methods.comparePassword = function(plaintextPassword, cb) {
+    bcrypt.compare(plaintextPassword, this.password, (err, isMatch) => {
+        if(err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
+UserSchema.methods.generateToken = function(cb){
+    var user = this;
+    var token = jwt.sign(this._id.toHexString(), 'secret')
+    user.token = token;
+    // console.log(token)
+    user.save(function(err, user){
+        if(err) return cb(err)
+        cb(null,user);
+    })
+}
+
+UserSchema.statics.findByToken = function (token, cb) {
+    const user = this;
+    jwt.verify(token, 'secret', (err, decoded) => {
+        user.findOne({"_id": decoded, "token": token}, function(err, user){
+            if(err) return cb(err);
+            cb(null, user);
+        })
+    })
+}
 
 const User = mongoose.model('User', UserSchema);
 
